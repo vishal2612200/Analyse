bdbfcefdfb0d390a952469aad7cf552207700367
vishal sharma <vishalsh533@gmail.com>
2018-08-30 15:47:57 +0530

    Update javascriprt1.js

--- javascriprt1.js
+++ javascriprt1.js
@@ -1,9 +1,7 @@
-
-
 function getParams(){
     var idx = document.URL.indexOf('?');
     var params = new Array();
-     if (idx != -1) {
+    if (idx != -1) {
         var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
         for (var i=0; i<pairs.length; i++){
             nameVal = pairs[i].split('=');
@@ -13,24 +11,10 @@ function getParams(){
     return params;
 }
 
-
     params = getParams();
-    input1 = unescape(params["input1"]);
-    input2 = unescape(params["input2"]);
-    input3 = unescape(params["input3"]);
-    input4 = unescape(params["input4"]);
-    input5 = unescape(params["input5"]);
-    input6 = unescape(params["input6"]);
-   var input1 = document.getElementById("lav").innerHTML= input1;
-    document.write("lastname = " + inputone + "<br>");
-  //  document.write("age = " + age + "<br>");
-
-   
-    document.getElementById("lav").innerHTML = input1;
-
-
-
-
-
     
 
+    for(var i=0;i<params.length;++i){
+        inputs[i]=unescape(params["input"+(i+1)]);
+    }
+  
